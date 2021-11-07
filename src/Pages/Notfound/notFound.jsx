68e2c2dcdae1b0cd1751834e550ab789cd3c8bbb
abrahamdecode <decodexdc@gmail.com>
2021-11-06 23:29:49 -0600

    se añadieron paginas de home y notfound, se renombro extension js por jsx

---
+++ src/Pages/Notfound/notFound.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const notFound = () => {
+    return (
+    <div className="not-found-container">
+        <p>Error 404</p>
+        <p>Yo habia Ponido mi pagina akii</p>
+
+    </div>
+    );
+}
+
+export default notFound;
